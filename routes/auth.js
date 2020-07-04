721ddba8ff35711600adcb19c3764d8aa01a87ad
tim <tjhickey@brandeis.edu>
2020-07-04 12:50:09 -0400

    removed debugging logging

--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,7 @@ const User = require('../models/User')
 // This is an example of middleware
 // where we look at a request and process it!
 router.use(function(req, res, next) {
-  //console.log("about to look for routes!!! "+new Date())
   console.log(`${req.method} ${req.url}`);
-  //console.dir(req.headers)
   next();
 });
 
@@ -24,7 +22,6 @@ router.use((req,res,next) => {
     res.locals.username = null
     res.locals.user = null
   }
-  console.log("res.locals = "+JSON.stringify(res.locals))
   next()
 })
 
@@ -40,9 +37,7 @@ router.post('/login',
       const hash = crypto.createHash('sha256');
       hash.update(passphrase);
       const encrypted = hash.digest('hex')
-      console.log(`hash.digest=${encrypted}`)
       const user = await User.findOne({username:username,passphrase:encrypted})
-      console.log(`user = ${user}`)
 
       if (user) {
         req.session.username = username //req.body
@@ -67,15 +62,11 @@ router.post('/signup',
       }else if (passphrase.split(' ').length < 5) {
         res.redirect('/login')
       }else {
-        console.log(`signing up ${username} with passphrase "${passphrase}"`)
         const hash = crypto.createHash('sha256');
         hash.update(passphrase);
         const encrypted = hash.digest('hex')
-        console.log(`hash.digest=${encrypted}`)
-        console.log(`hash.digest=${encrypted}`)
         const user = new User({username:username,passphrase:encrypted})
         await user.save()
-        console.log('saved info in User')
         req.session.username = user.username
         req.session.user = user
         res.redirect('/')
@@ -86,7 +77,7 @@ router.post('/signup',
   })
 
 router.get('/logout', (req,res) => {
-  req.session.destroy() //(error)=>{console.log("Error in destroying session: "+error)});
+  req.session.destroy()
   res.redirect('/');
 })
 
